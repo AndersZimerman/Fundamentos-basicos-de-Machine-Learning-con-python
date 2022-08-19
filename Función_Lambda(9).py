facfd8af367af6ae0245d8091d0980897778a5f8
AndersZimerman <111028542+AndersZimerman@users.noreply.github.com>
2022-08-19 09:23:04 -0500

    Update Función_Lambda(9).py

--- Función_Lambda(9).py
+++ Función_Lambda(9).py
@@ -3,9 +3,6 @@
 #Se utiliza mayormente para simplificar codigo
 #Sintaxis: lambda argumento1, argumento2 : expresión (cuerpo de la función)
 #Ejemplo (sin lambda):
-from unicodedata import name
-
-
 def doble(x):
     return x * 2
 print(doble(200))
@@ -24,11 +21,3 @@ print(listax2)
 lista = [0,1,-2,3,-4,5,-6,7]
 numeros_positivos = list(filter(lambda x: x > 0,lista))
 print(numeros_positivos)
-#Multiplicar varios numeros:
-def multiplicar_por (n):
-  return lambda x: x * n
-  a = print((input('a')))
-  b = list(map(lambda,a))
-duplicar = multiplicar_por(2)
-triplicar = multiplicar_por(3)
-diez_veces = multiplicar_por(10)
